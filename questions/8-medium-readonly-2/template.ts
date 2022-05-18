4e64e67cf421cd145f061b46c4673a682badae95
陈化宇 <1171193188@qq.com>
2022-05-18 18:05:45 +0800

    feat: add questions

--- questions/8-medium-readonly-2/template.ts
+++ questions/8-medium-readonly-2/template.ts
@@ -1,1 +1,19 @@
-type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T,K> & Readonly<Pick<T, K>>
+// 使用工具类解决
+// type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T,K> & Readonly<Pick<T, K>>
+
+// 简化版
+// type MyReadonly2<T, K extends keyof T = keyof T> = {readonly [key in K]: T[key]} & {[key in keyof T as (key extends K ? never : key)]: T[key]}
+
+// 详细版
+type MyEncloud<T, K> = T extends K ? never : T; 
+type MyReadonly2<T, K extends keyof T = keyof T> = {readonly [key in K]: T[key]} & {[key in MyEncloud<keyof T, K>]: T[key]}
+
+
+// 知识点：
+// 1、泛型 K 存在空的情况， 所以采用 = 默认赋值
+// 2、as 指定类型，“ 此用法会阻断编译器的类型推断，需谨慎使用 ”
+// 3、& 表示交集，两个类型都拥有的类型
+
+// 步骤：
+// 1、重置对象中所有的属性为 Readonly 类型
+// 2、如果置顶类型，则相关类型为 Readonly 类型；否则全部为 Readonly 类型。
